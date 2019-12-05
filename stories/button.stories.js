1b5073b167ca2684dc701b444c9ed42ba2cf1365
Ade Novid <adenvt@gmail.com>
2019-12-05 16:34:23 +0700

    added: storybook addons note

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -4,23 +4,27 @@ import variants from './knobs/variants'
 import size from './knobs/size'
 
 storiesOf('Components', module)
-  .add('dButton', () => ({
-    props: {
-      variants: { default: variants() },
-      size    : { default: size() },
-      icon    : { default: boolean('Icon', false) },
-      iconOnly: { default: boolean('Icon Only', false) },
-    },
-    render (h) {
-      return (
-        <d-btn
-          variant={this.variants}
-          size={this.size}
-          icon={this.icon ? 'add' : ''}
-          icon-only={this.iconOnly}
-        >
-          Button
-        </d-btn>
-      )
-    },
-  }))
+  .add(
+    'dButton',
+    () => ({
+      props: {
+        variants: { default: variants() },
+        size    : { default: size() },
+        icon    : { default: boolean('Icon', false) },
+        iconOnly: { default: boolean('Icon Only', false) },
+      },
+      render (h) {
+        return (
+          <d-btn
+            variant={this.variants}
+            size={this.size}
+            icon={this.icon ? 'add' : ''}
+            icon-only={this.iconOnly}
+          >
+            Button
+          </d-btn>
+        )
+      },
+    }),
+    { notes: 'A very simple example of addon notes' },
+  )
